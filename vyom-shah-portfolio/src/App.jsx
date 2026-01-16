b570f5039e9980ad027deba89c7fec14427b634e
vyomshah05 <vyomshah005@gmail.com>
2026-01-16 12:43:32 -0800

    Portfolio started with Hero, About Me, and Education

---
+++ vyom-shah-portfolio/src/App.jsx
@@ -0,0 +1,14 @@
+import Hero from "./components/Hero";
+import About from "./components/About"
+import Education from "./components/Education";
+import "./styles/global.css";
+
+export default function App() {
+  return (
+    <>
+      <Hero />
+      <About />
+      <Education />
+    </>
+  )
+}
